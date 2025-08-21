abc39e0207744757e093b8ae7932beb0c5d68b15
Oscar1640 <92490286+Oscar1640@users.noreply.github.com>
2025-08-21 02:59:08 -0500

    delete old files

--- genie_history.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ../mame
-git log -p makefile scripts/{extlib.lua,genie.lua,toolchain.lua} scripts/src/3rdparty.lua scripts/src/osd/*
--- update_tree.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir 3rdparty
-rsync -r --delete ../mame/3rdparty/{expat,libflac,lzma,nanosvg,utf8proc,zlib} 3rdparty/
-rsync -r --delete ../mame/src/{emu,frontend,lib} src/
-rsync -r --delete --exclude="modules/lib/osd_getenv.cpp" ../mame/src/osd src/
-mkdir src/tools/
-rsync ../mame/src/tools/chdman.cpp src/tools/
-
